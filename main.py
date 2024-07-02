8a3533b59347f194f86a6e5728880fe99d517de9
alanlevyy <alanlevy2009@hotmail.com>
2024-07-02 19:33:00 -0300

    segundopush

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu
 
-st.title('Hello, Streamlit!')
-st.write('This is a test application.')
+data = pd.read_csv('df_final_limpio.csv')
+
+#2. Titulo de pagina
+st.set_page_config(page_title="Sistema de recomendación de Recetas")
+
+#3. Sidebar
+with st.sidebar:
+    selected = option_menu(
+        menu_title='Menu',
+        options=['Home', 'Visualizando los datos', 'Armado del modelo', 'Encontrá tu libro'],
+    )
+
